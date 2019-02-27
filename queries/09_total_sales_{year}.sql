b2a786c49a820b586e5de2aad4b371e616908adc
robbyhecht <robbyhecht@gmail.com>
2019-02-26 21:25:03 -0600

    moderate changes to queries 8 and 9

--- queries/09_total_sales_{year}.sql
+++ queries/09_total_sales_{year}.sql
@@ -1,8 +1,8 @@
 -- total_sales_{year}.sql: What are the respective total sales for each of those years?
 
-SELECT ROUND(SUM (Total), 2)
+SELECT STRFTIME('%Y', Invoice.InvoiceDate) as Year, ROUND(SUM (Total), 2) as "Total Sales"
 FROM Invoice
-WHERE InvoiceDate BETWEEN '2009-01-01 00:00:00' and '2011-12-31 00:00:00' 
-AND InvoiceDate NOT BETWEEN '2010-01-01 00:00:00' and '2010-12-31 00:00:00'
+WHERE Year = '2009' or Year = '2011'
+GROUP BY Year
 
 -- with the ROUND statement, the sum is rounded to the number of decimals declared by the number after the comma... in this case 2.
